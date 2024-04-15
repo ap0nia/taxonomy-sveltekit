8bada49961212dd1d11716fee83d44e76b9fda52
Aponia <brianmv@uci.edu>
2024-04-15 11:13:50 -0700

    feat(ui): ✨ shadcn avatar component

---
+++ packages/ui/src/lib/components/ui/avatar/avatar-fallback.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Avatar as AvatarPrimitive } from 'bits-ui'
+
+  import { cn } from '$lib/utils/cn'
+
+  type $$Props = AvatarPrimitive.FallbackProps
+
+  let className: $$Props['class'] = undefined
+
+  export { className as class }
+</script>
+
+<AvatarPrimitive.Fallback
+  class={cn('flex h-full w-full items-center justify-center rounded-full', className)}
+  {...$$restProps}
+>
+  <slot />
+</AvatarPrimitive.Fallback>
