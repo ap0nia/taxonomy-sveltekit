6412a2c7d087b9d8211be3bed131b187cd4e7081
Aponia <brianmv@uci.edu>
2024-04-16 16:34:23 -0700

    chore(editor): 🔧 simplify language store

--- apps/editor/src/lib/config/language.ts
+++ apps/editor/src/lib/config/language.ts
@@ -1,43 +1,26 @@
-import type { Selected } from 'bits-ui'
 import { writable } from 'svelte/store'
 
+import { browser } from '$app/environment'
 import { t } from '$lib/i18n'
-import * as m from '$paraglide/messages'
-import {
-  type AvailableLanguageTag,
-  languageTag,
-  onSetLanguageTag,
-  setLanguageTag,
-} from '$paraglide/runtime'
+import { languageTag, onSetLanguageTag } from '$paraglide/runtime'
 
 /**
  * Don't use this store because {@link languageTag} only works fully correctly within components.
  * This is a convenience observable that just provides updates.
+ *
+ * @internal
  */
 export function createLanguageStore() {
-  const value = languageTag()
-
-  const label = m.__name({}, { languageTag: value })
-
-  const store = writable({ label, value }, (set) => {
-    if (typeof document === 'undefined') return
+  const store = writable(languageTag(), (set) => {
+    if (!browser) return
 
     onSetLanguageTag((newLanguageTag) => {
-      const label = m.__name({ language: newLanguageTag })
-      set({ label, value: newLanguageTag })
+      set(newLanguageTag)
     })
   })
 
-  /**
-   * Don't directly set the store, the store will listen to changes to the paraglide store.
-   */
-  const set = (selected: Selected<AvailableLanguageTag>) => {
-    setLanguageTag(selected.value)
-  }
-
   return {
     ...store,
-    set,
   }
 }
 
