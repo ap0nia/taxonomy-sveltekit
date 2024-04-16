b3c777a97a8c4d7786aae3482fb1d1f39e7b6656
Aponia <brianmv@uci.edu>
2024-04-16 08:16:10 -0700

    feat(editor): ✨ social links for website config

--- apps/editor/src/lib/config/site.ts
+++ apps/editor/src/lib/config/site.ts
@@ -1,1 +1,13 @@
 export const title = 'Taxonomy'
+
+export const description =
+  'An open source application built using the new router, server components and everything new in Next.js 13.'
+
+export const socials = {
+  twitter: {
+    url: 'https://twitter.com/shadcn',
+  },
+  github: {
+    url: 'https://github.com/shadcn/taxonomy',
+  },
+}
--- apps/editor/src/routes/(main)/+page.svelte
+++ apps/editor/src/routes/(main)/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { socials } from '$lib/config/site'
 </script>
 
 <div class="w-full my-8">
@@ -6,18 +7,27 @@
   <section class="hero py-32">
     <div class="hero-content">
       <div class="max-w-5xl space-y-4 text-center">
+        <a href={socials.twitter.url} target="_blank" class="badge badge-neutral badge-lg">
+          Follow along on Twitter
+        </a>
+
         <h1 class="font-heading font-bold text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
           An example app built using Next.js 13 server components.
         </h1>
 
-        <p class="max-w-3xl mx-auto leading-normal sm:text-xl sm:leading-8">
+        <p class="max-w-3xl mx-auto opacity-70 sm:text-xl leading-normal sm:leading-8">
           I&apos;m building a web app with Next.js 13 and open sourcing everything. Follow along as
           we figure this out together.
         </p>
 
         <div class="space-x-4">
-          <a href="/" class="btn btn-primary lg:btn-wide">Get Started</a>
-          <a href="/" class="btn btn-outline lg:btn-wide">GitHub</a>
+          <a href="/auth/login" class="btn btn-primary lg:btn-wide">Get Started</a>
+          <a
+            href={socials.github.url}
+            target="_blank"
+            rel="noreferrer"
+            class="btn btn-outline lg:btn-wide">GitHub</a
+          >
         </div>
       </div>
     </div>
