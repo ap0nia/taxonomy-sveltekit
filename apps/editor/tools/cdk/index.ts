19701f04b98fccb62d8903c674f2c8d5d5873c30
Aponia <brianmv@uci.edu>
2024-04-16 10:25:52 -0700

    chore(editor): 🔧 update cdk deploy script

--- apps/editor/tools/cdk/index.ts
+++ apps/editor/tools/cdk/index.ts
@@ -9,13 +9,6 @@ config({
 })
 
 const envSchema = Type.Object({
-  SENTRY_AUTH_TOKEN: Type.String(),
-  PUBLIC_CHAT_API_URL: Type.String(),
-  PUBLIC_PUSHER_KEY: Type.String(),
-  PUBLIC_PUSHER_CLUSTER: Type.String(),
-  PUSHER_APP_ID: Type.String(),
-  PUSHER_SECRET: Type.String(),
-  DATABASE_URL: Type.String(),
   TURSO_CONNECTION_URL: Type.String(),
   TURSO_AUTH_TOKEN: Type.String(),
   GOOGLE_ID: Type.String(),
@@ -26,7 +19,6 @@ const envSchema = Type.Object({
   GITHUB_SECRET: Type.String(),
   GITHUB_ID_PRODUCTION: Type.String(),
   GITHUB_SECRET_PRODUCTION: Type.String(),
-  OPENAI_API_KEY: Type.String(),
   GOOGLE_CREDENTIALS_JSON: Type.String(),
 })
 
@@ -42,7 +34,7 @@ const env = cleanEnv
 async function main() {
   const app = new App()
 
-  const stack = new Stack(app, 'baymax')
+  const stack = new Stack(app, 'taxonomy')
 
   const sveltekit = new SvelteKit(stack, 'SvelteKit', {
     constructProps: {
