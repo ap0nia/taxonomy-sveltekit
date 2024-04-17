6bb5a312213f4cd83b104b5bfa04ab01628767ce
Aponia <brianmv@uci.edu>
2024-04-16 22:04:28 -0700

    fix(editor): 🐛 container max width

--- apps/editor/src/routes/(contentlayer)/documentation/+layout.svelte
+++ apps/editor/src/routes/(contentlayer)/documentation/+layout.svelte
@@ -8,7 +8,9 @@
 <div class="flex flex-col min-h-screen">
   <Header />
 
-  <div class="flex-1 md:grid md:grid-cols-[220px_1fr] md:gap-6 lg:grid-cols-[240px_1fr] lg:gap-10">
+  <div
+    class="container mx-auto flex-1 md:grid md:grid-cols-[220px_1fr] md:gap-6 lg:grid-cols-[240px_1fr] lg:gap-10"
+  >
     <aside
       class="fixed top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r py-6 pr-2 md:sticky md:block lg:py-10"
     >
