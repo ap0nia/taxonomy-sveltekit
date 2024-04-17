a8e2f30682be9731a5fe23039aee563fcbd84013
Aponia <brianmv@uci.edu>
2024-04-16 20:02:45 -0700

    feat(editor): ✨ pricing page

--- apps/editor/src/routes/(main)/pricing/+page.svelte
+++ apps/editor/src/routes/(main)/pricing/+page.svelte
@@ -1,46 +1,47 @@
 <script lang="ts">
   import CheckIcon from '~icons/mdi/check'
+  import { messages } from '$lib/config/language'
 </script>
 
 <main class="container md:max-w-5xl px-4 py-12 lg:py-24 mx-auto space-y-4">
   <div class="space-y-2">
-    <h2 class="font-heading text-3xl md:text-6xl">Simple, transparent pricing</h2>
-    <p class="sm:text-lg">Unlock all features including unlimited posts for your blog.</p>
+    <h2 class="font-heading text-3xl md:text-6xl">{$messages('simple_pricing')}</h2>
+    <p class="sm:text-lg">{$messages('unlock_all_features')}.</p>
   </div>
 
   <div class="w-full p-10 grid md:grid-cols-[1fr_200px] items-start gap-10 rounded-lg border">
     <div class="grid gap-6">
-      <h3 class="font-bold text-xl sm:text-2xl">What&apos;s included in the PRO plan</h3>
+      <h3 class="font-bold text-xl sm:text-2xl">{$messages('whats_included_pro')}</h3>
 
       <ul class="grid gap-3 text-sm sm:grid-cols-2">
         <li class="flex items-center">
           <CheckIcon class="mr-2 h-4 w-4" />
-          <span>Unlimited Posts</span>
+          <span>{$messages('unlimited_posts')}</span>
         </li>
 
         <li class="flex items-center">
           <CheckIcon class="mr-2 h-4 w-4" />
-          <span>Unlimited Users</span>
+          <span>{$messages('unlimited_users')}</span>
         </li>
 
         <li class="flex items-center">
           <CheckIcon class="mr-2 h-4 w-4" />
-          <span>Custom domain</span>
+          <span>{$messages('custom_domain')}</span>
         </li>
 
         <li class="flex items-center">
           <CheckIcon class="mr-2 h-4 w-4" />
-          <span>Dashboard Analytics</span>
+          <span>{$messages('dashboard_analytics')}</span>
         </li>
 
         <li class="flex items-center">
           <CheckIcon class="mr-2 h-4 w-4" />
-          <span>Access to Discord</span>
+          <span>{$messages('access_discord')}</span>
         </li>
 
         <li class="flex items-center">
           <CheckIcon class="mr-2 h-4 w-4" />
-          <span>Premium Support</span>
+          <span>{$messages('premium_support')}</span>
         </li>
       </ul>
     </div>
@@ -48,16 +49,16 @@
     <div class="flex flex-col gap-4 text-center">
       <div>
         <h4 class="text-7xl font-bold">$19</h4>
-        <p class="text-sm font-medium">Billed Monthly</p>
+        <p class="text-sm font-medium">{$messages('billed_monthly')}</p>
       </div>
-      <a href="/auth/login" class="btn btn-primary">Get Started</a>
+      <a href="/auth/login" class="btn btn-primary">{$messages('get_started')}</a>
     </div>
   </div>
 
   <div class="mx-auto flex w-full max-w-[58rem] flex-col gap-4">
     <p class="max-w-[85%] leading-normal text-muted-foreground sm:leading-7">
-      Taxonomy is a demo app.{' '}
-      <strong>You can test the upgrade and won&apos;t be charged.</strong>
+      <span>{$messages('is_demo_app')}.</span>
+      <strong>{$messages('you_can_test')}.</strong>
     </p>
   </div>
 </main>
